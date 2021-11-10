2d756e6f87d034ba69fd397097bedefab1297fd3
shirinstar <shirinanlen@gmail.com>
2021-11-10 16:02:17 -0500

    hole in the wall

--- 4_arjs_insideCube/src/script.js
+++ 4_arjs_insideCube/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const sizes = {
   width: window.innerWidth,
@@ -16,12 +16,13 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width * 2 / sizes.height, 0.1, 1000);
 camera.position.z = 0
-// camera.rotation.y = -10
 scene.add(camera);
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -83,7 +84,6 @@ arToolkitContext.init(function onCompleted() {
   camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
 });
 
-
 // setup markerRoots and build markerControls
 const markerRoot = new THREE.Group();
 scene.add(markerRoot);
@@ -95,119 +95,125 @@ let markerControls = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
 
 //scene content
 
-	// the inside of the hole
-	let geometry1	= new THREE.BoxBufferGeometry(2,2,2);
-	
-  const textureLoader = new THREE.TextureLoader()
-  const texture = textureLoader.load('./tiles.jpeg')
-	let material1	= new THREE.MeshBasicMaterial({
-		transparent : true,
-    // color: 0xff0000,
-		map: texture,
-		side: THREE.BackSide
-	}); 
-	
-	const mesh1 = new THREE.Mesh( geometry1, material1 );
-	mesh1.position.y = -1;
-  mesh1.rotation.x = 40
-	
-	markerRoot.add( mesh1 );
-	
-	// the invisibility cloak (box with a hole)
-	//let geometry0 = new THREE.BoxBufferGeometry(2,2,2);
-  //console.log(geometry0);
-	//geometry0.groups.splice(4, 2); // make hole by removing top two triangles
+// the inside of the hole
+let insideBoxGeometry = new THREE.BoxBufferGeometry(2, 2, 2);
 
-  const vertices = [
-    // front
-    { pos: [-1, -1,  1], norm: [ 0,  0,  1], uv: [0, 0], },
-    { pos: [ 1, -1,  1], norm: [ 0,  0,  1], uv: [1, 0], },
-    { pos: [-1,  1,  1], norm: [ 0,  0,  1], uv: [0, 1], },
-   
-    { pos: [-1,  1,  1], norm: [ 0,  0,  1], uv: [0, 1], },
-    { pos: [ 1, -1,  1], norm: [ 0,  0,  1], uv: [1, 0], },
-    { pos: [ 1,  1,  1], norm: [ 0,  0,  1], uv: [1, 1], },
-    // right
-    { pos: [ 1, -1,  1], norm: [ 1,  0,  0], uv: [0, 0], },
-    { pos: [ 1, -1, -1], norm: [ 1,  0,  0], uv: [1, 0], },
-    { pos: [ 1,  1,  1], norm: [ 1,  0,  0], uv: [0, 1], },
-   
-    { pos: [ 1,  1,  1], norm: [ 1,  0,  0], uv: [0, 1], },
-    { pos: [ 1, -1, -1], norm: [ 1,  0,  0], uv: [1, 0], },
-    { pos: [ 1,  1, -1], norm: [ 1,  0,  0], uv: [1, 1], },
-    // back
-    { pos: [ 1, -1, -1], norm: [ 0,  0, -1], uv: [0, 0], },
-    { pos: [-1, -1, -1], norm: [ 0,  0, -1], uv: [1, 0], },
-    { pos: [ 1,  1, -1], norm: [ 0,  0, -1], uv: [0, 1], },
-   
-    { pos: [ 1,  1, -1], norm: [ 0,  0, -1], uv: [0, 1], },
-    { pos: [-1, -1, -1], norm: [ 0,  0, -1], uv: [1, 0], },
-    { pos: [-1,  1, -1], norm: [ 0,  0, -1], uv: [1, 1], },
-    // // left
-    { pos: [-1, -1, -1], norm: [-1,  0,  0], uv: [0, 0], },
-    { pos: [-1, -1,  1], norm: [-1,  0,  0], uv: [1, 0], },
-    { pos: [-1,  1, -1], norm: [-1,  0,  0], uv: [0, 1], },
-   
-    { pos: [-1,  1, -1], norm: [-1,  0,  0], uv: [0, 1], },
-    { pos: [-1, -1,  1], norm: [-1,  0,  0], uv: [1, 0], },
-    { pos: [-1,  1,  1], norm: [-1,  0,  0], uv: [1, 1], },
-    // // top
-    // { pos: [ 1,  1, -1], norm: [ 0,  1,  0], uv: [0, 0], },
-    // { pos: [-1,  1, -1], norm: [ 0,  1,  0], uv: [1, 0], },
-    // { pos: [ 1,  1,  1], norm: [ 0,  1,  0], uv: [0, 1], },
-   
-    // { pos: [ 1,  1,  1], norm: [ 0,  1,  0], uv: [0, 1], },
-    // { pos: [-1,  1, -1], norm: [ 0,  1,  0], uv: [1, 0], },
-    // { pos: [-1,  1,  1], norm: [ 0,  1,  0], uv: [1, 1], },
-    // // bottom
-    // { pos: [ 1, -1,  1], norm: [ 0, -1,  0], uv: [0, 0], },
-    // { pos: [-1, -1,  1], norm: [ 0, -1,  0], uv: [1, 0], },
-    // { pos: [ 1, -1, -1], norm: [ 0, -1,  0], uv: [0, 1], },
-   
-    // { pos: [ 1, -1, -1], norm: [ 0, -1,  0], uv: [0, 1], },
-    // { pos: [-1, -1,  1], norm: [ 0, -1,  0], uv: [1, 0], },
-    // { pos: [-1, -1, -1], norm: [ 0, -1,  0], uv: [1, 1], },
-  ];
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('./tiles.jpeg')
+let insideBoxMaterial	= new THREE.MeshBasicMaterial({
+	transparent : true,
+  // color: 0xff0000,
+	map: texture,
+	side: THREE.BackSide //rendering only in the inside geo
+}); 
 
+// const insideBoxMaterial = new THREE.MeshNormalMaterial({
+//   transparent: true,
+//   // opacity: 0.5,
+//   side: THREE.BackSide //rendering only in the inside geo
+// });
 
-  const positions = [];
-  const normals = [];
-  const uvs = [];
-  for (const vertex of vertices) {
-    positions.push(...vertex.pos);
-    normals.push(...vertex.norm);
-    uvs.push(...vertex.uv);
-  }
+const insideBox = new THREE.Mesh(insideBoxGeometry, insideBoxMaterial);
+//insideBox.position.y = -1;
+insideBox.rotation.x = Math.PI * 0.25
 
-  const geometry0 = new THREE.BufferGeometry();
-  const positionNumComponents = 3;
-  const normalNumComponents = 3;
-  const uvNumComponents = 2;
-  geometry0.setAttribute(
-      'position',
-      new THREE.BufferAttribute(new Float32Array(positions), positionNumComponents));
-      geometry0.setAttribute(
-      'normal',
-      new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents));
-      geometry0.setAttribute(
-      'uv',
-      new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents));
+markerRoot.add(insideBox);
 
-	
-      let material0 = new THREE.MeshBasicMaterial({
-        colorWrite: false
-      });
-      
-      let mesh0 = new THREE.Mesh( geometry0, material0 );
-      mesh0.scale.set(1,1,1).multiplyScalar(1.01);
-      mesh0.position.y = -1;
-      mesh0.rotation.x = 40
-    
+// the invisibility cloak (box with a hole) 
+//- building box vertices as a buffer geo so we can get rid of faces  
+const vertices = [
+  // front
+  { pos: [-1, -1, 1], norm: [0, 0, 1], uv: [0, 0], },
+  { pos: [1, -1, 1], norm: [0, 0, 1], uv: [1, 0], },
+  { pos: [-1, 1, 1], norm: [0, 0, 1], uv: [0, 1], },
 
-	markerRoot.add(mesh0);	
+  { pos: [1, 1, 1], norm: [0, 0, 1], uv: [1, 1], },
+  // right
+  { pos: [1, -1, 1], norm: [1, 0, 0], uv: [0, 0], },
+  { pos: [1, -1, -1], norm: [1, 0, 0], uv: [1, 0], },
+  { pos: [1, 1, 1], norm: [1, 0, 0], uv: [0, 1], },
+
+  { pos: [1, 1, -1], norm: [1, 0, 0], uv: [1, 1], },
+  // back
+  { pos: [1, -1, -1], norm: [0, 0, -1], uv: [0, 0], },
+  { pos: [-1, -1, -1], norm: [0, 0, -1], uv: [1, 0], },
+  { pos: [1, 1, -1], norm: [0, 0, -1], uv: [0, 1], },
+
+  { pos: [-1, 1, -1], norm: [0, 0, -1], uv: [1, 1], },
+  // // left
+  { pos: [-1, -1, -1], norm: [-1, 0, 0], uv: [0, 0], },
+  { pos: [-1, -1, 1], norm: [-1, 0, 0], uv: [1, 0], },
+  { pos: [-1, 1, -1], norm: [-1, 0, 0], uv: [0, 1], },
+
+  { pos: [-1, 1, 1], norm: [-1, 0, 0], uv: [1, 1], },
+
+  // // top 
+  // { pos: [ 1,  1, -1], norm: [ 0,  1,  0], uv: [0, 0], },
+  // { pos: [-1,  1, -1], norm: [ 0,  1,  0], uv: [1, 0], },
+  // { pos: [ 1,  1,  1], norm: [ 0,  1,  0], uv: [0, 1], },
+
+  // { pos: [-1,  1,  1], norm: [ 0,  1,  0], uv: [1, 1], },
+  // // bottom
+  { pos: [1, -1, 1], norm: [0, -1, 0], uv: [0, 0], },
+  { pos: [-1, -1, 1], norm: [0, -1, 0], uv: [1, 0], },
+  { pos: [1, -1, -1], norm: [0, -1, 0], uv: [0, 1], },
+
+  { pos: [-1, -1, -1], norm: [0, -1, 0], uv: [1, 1], },
+];
+
+const positions = [];
+const normals = [];
+const uvs = [];
+
+for (const vertex of vertices) {
+  positions.push(...vertex.pos);
+  normals.push(...vertex.norm);
+  uvs.push(...vertex.uv);
+}
+
+const invisbleBoxGeometry = new THREE.BufferGeometry();
+const positionNumComponents = 3;
+const normalNumComponents = 3;
+const uvNumComponents = 2;
+
+invisbleBoxGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(new Float32Array(positions), positionNumComponents));
+
+invisbleBoxGeometry.setAttribute(
+  'normal',
+  new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents));
+
+invisbleBoxGeometry.setAttribute(
+  'uv',
+  new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents));
 
 
+let invisbleBoxMaterial = new THREE.MeshBasicMaterial({
+  colorWrite: false //Whether to render the material's color 
+});
 
+let invisbleBox = new THREE.Mesh(invisbleBoxGeometry, invisbleBoxMaterial);
+invisbleBox.scale.set(1, 1, 1).multiplyScalar(1.01);
+//invisbleBox.position.y = -1;
+invisbleBox.rotation.x = Math.PI * 0.25
+
+markerRoot.add(invisbleBox);
+
+//flower model inside the box
+//   const gltfLoader = new GLTFLoader()
+
+//   gltfLoader.load(
+//     '/tulip.glb',
+//     (gltf) =>
+//     {
+//       gltf.scene.rotation.x = Math.PI * 0.25
+//       gltf.scene.position.z =1
+//       gltf.scene.position.y =1
+//       gltf.scene.scale.set(0.25, 0.25, 0.25)
+//       markerRoot.add(gltf.scene)
+//     }
+// )
 
 const update = () => {
   // update artoolkit on every frame
@@ -221,7 +227,7 @@ const render = () => {
 }
 
 const animate = () => {
-  // controls.update()
+  controls.update()
 
   requestAnimationFrame(animate);
   update();
