787200ade8e80690aea69f523caa199b2b52831a
shirinstar <shirinanlen@gmail.com>
2021-12-10 13:58:38 -0500

    contast greyscale

--- 11_arjs_accessibility_contrast/src/shader/fragment.glsl
+++ 11_arjs_accessibility_contrast/src/shader/fragment.glsl
@@ -4,21 +4,16 @@ varying vec2 vUv;
 
 void main()
 {   
-   ///add mix function so it wont be such a rough transiton
-    //vec3 color = mix(ucolor, newcolor, videocolor.r -- this needs to be float);
-
     vec4 videoColor = texture2D( uTexture , vUv + 1.5);
     
     vec3 newColor = uColor;
-    vec3 strength = vec3(0.5);
-
-    //if the video feed is too drak -> make the text brighter
-   
+    vec3 strength = vec3(0.5);   
 
     newColor.r = 1.0-step(0.6, videoColor.r);
     newColor.g = 1.0-step(0.6, videoColor.g);
     newColor.b = 1.0-step(0.6, videoColor.b);
 
+    //if the video feed is too drak -> make the distance calc smaller/bigger
      if(newColor.r < 0.5 && newColor.g < 0.5 && newColor.b < 0.5) {
      strength = vec3(0.2);
     }
@@ -26,9 +21,7 @@ void main()
       strength = vec3(0.4);
     }
     
-    float blancedColor = distance(newColor, strength);
+    float balancedColor = distance(newColor, strength);
 
-
-    gl_FragColor = vec4(vec3(blancedColor), 1.0);
-  
+    gl_FragColor = vec4(vec3(balancedColor), 1.0);
 }
